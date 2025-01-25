3f7fa73f23317b4586c35a02df2456bef068d9eb
thomas-pilot-in <thomas@pilot-in.com>
2025-01-25 12:20:57 +0100

    init: button counter component

---
+++ my-vue-app/src/components/ButtonComponent.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name:"ButtonComponent",
+    props:{
+        label: {
+            type: String , 
+            required:true,
+        },
+    },
+    emits:["click"],
+    setup(_, {emit}) {
+        const handleClick = () => {
+            emit("click");
+        };
+
+        return {
+            handleClick,
+        };
+    },
+});
+
+</script>
+
+<template>
+    <button @click="handleClick">
+        {{ label }}
+    </button>
+</template>
--- my-vue-app/src/components/HelloWorld.vue
+++ my-vue-app/src/components/HelloWorld.vue
@@ -1,41 +1,48 @@
-<script setup lang="ts">
-import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import ButtonComponent from './ButtonComponent.vue';
 
-const count = ref(0)
+export default defineComponent({
+  name: "HelloWorld",
+  components: {
+    ButtonComponent,
+  },
+  setup() {
+    const message = ref<string>("Hello Vue 3 with TypeScript!");
+    const count = ref<number>(0);
+    const increment  = () => {
+      count.value++;
+    };
+
+    const clickCount = ref<number>(0);
+
+    const handleButtonClick = () => {
+      clickCount.value++;
+    }
+
+    return {
+      message,
+      count,
+      increment,
+      clickCount,
+      handleButtonClick,
+    };
+  },
+
+});
+
 </script>
 
+
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="increment">Count is:{{ count }}</button> 
+    <button-component label="Click me!" @click="handleButtonClick" />
+    <p>You clicked the button {{ clickCount }} times.</p>
+   </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+
+
